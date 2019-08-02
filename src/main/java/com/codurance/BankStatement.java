f1d607b26a3086d950b8e38abaabce65df5b5a03
AurelijaZuba <auryte00@hotmail.com>
2019-08-02 11:56:45 +0100

    Passing test for generating hard coded statement

--- src/main/java/com/codurance/BankStatement.java
+++ src/main/java/com/codurance/BankStatement.java
@@ -2,8 +2,13 @@ package com.codurance;
 
 import java.util.List;
 
+import static java.util.Arrays.asList;
+
 public class BankStatement {
     public List<String> generate(List<Integer> transactions) {
-        throw new UnsupportedOperationException();
+        return asList(
+                "Date || Amount || Balance",
+                "02/08/2019 || -10 || 0",
+                "02/08/2019 || 10  || 10");
     }
 }
