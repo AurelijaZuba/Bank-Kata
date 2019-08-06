64fc19982c221602269d3a9c2c48ffa4ef1b2d08
AurelijaZuba <auryte00@hotmail.com>
2019-08-06 14:54:45 +0100

    refactoring amount to a constant field

--- src/test/java/com/codurance/Unit/TransactionRecordShould.java
+++ src/test/java/com/codurance/Unit/TransactionRecordShould.java
@@ -14,50 +14,48 @@ import static org.mockito.Mockito.verify;
 
 public class TransactionRecordShould {
 
+    public static final int AMOUNT = 30;
+
     @Test
     void add_deposit() {
-        int amount = 30;
         List<Transaction> transactionList = mock(List.class);
 
         TransactionRecord transactionRecord = new TransactionRecord(transactionList);
-        transactionRecord.addDeposit(amount);
+        transactionRecord.addDeposit(AMOUNT);
 
         verify(transactionList).add(isA(Transaction.class));
     }
 
     @Test
     void add_withdraw() {
-        int amount = 30;
         List<Transaction> transactionList = mock(List.class);
 
         TransactionRecord transactionRecord = new TransactionRecord(transactionList);
-        transactionRecord.addWithdraw(amount);
+        transactionRecord.addWithdraw(AMOUNT);
 
         verify(transactionList).add(isA(Transaction.class));
     }
 
     @Test
     void add_deposit_transaction_with_correct_amount() {
-        int amount = 30;
         List<Transaction> transactionList = mock(List.class);
 
         TransactionRecord transactionRecord = new TransactionRecord(transactionList);
-        transactionRecord.addDeposit(amount);
+        transactionRecord.addDeposit(AMOUNT);
 
         ArgumentCaptor<Transaction> argument = ArgumentCaptor.forClass(Transaction.class);
         verify(transactionList).add(argument.capture());
 
-        assertThat(argument.getValue().amount()).isEqualTo(amount);
+        assertThat(argument.getValue().amount()).isEqualTo(AMOUNT);
     }
 
     @Test
     void add_deposit_transaction_with_correct_balance() {
-        int amount = 30;
         int balance = 30;
         List<Transaction> transactionList = mock(List.class);
 
         TransactionRecord transactionRecord = new TransactionRecord(transactionList);
-        transactionRecord.addDeposit(amount);
+        transactionRecord.addDeposit(AMOUNT);
 
         ArgumentCaptor<Transaction> argument = ArgumentCaptor.forClass(Transaction.class);
         verify(transactionList).add(argument.capture());
