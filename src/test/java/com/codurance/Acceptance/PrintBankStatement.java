65a2d6306bf3c7454b3e015e9c0b9a800b13d1ef
AurelijaZuba <auryte00@hotmail.com>
2019-08-06 11:26:56 +0100

    Refactoring to parallel changes test for transaction record

--- src/test/java/com/codurance/Acceptance/PrintBankStatement.java
+++ src/test/java/com/codurance/Acceptance/PrintBankStatement.java
@@ -3,6 +3,7 @@ package com.codurance.Acceptance;
 import com.codurance.AccountService;
 import com.codurance.BankStatement;
 import com.codurance.ConsoleWrite;
+import com.codurance.TransactionRecord;
 import org.junit.jupiter.api.Test;
 import org.mockito.InOrder;
 
@@ -25,6 +26,28 @@ public class PrintBankStatement {
         accountService.deposit(2000);
         accountService.withdraw(500);
         accountService.printStatement();
+
+
+        InOrder inOrder = inOrder(consoleMock);
+        inOrder.verify(consoleMock).print("Date || Amount || Balance");
+        inOrder.verify(consoleMock).print("14/01/2012 || -500   || 2500");
+        inOrder.verify(consoleMock).print("13/01/2012 || 2000   || 3000");
+        inOrder.verify(consoleMock).print("10/01/2012 || 1000   || 1000");
+
+    }
+
+
+    @Test
+    void AcceptanceTest1() {
+        ConsoleWrite consoleMock = mock(ConsoleWrite.class);
+        BankStatement bankStatement = new BankStatement();
+
+        TransactionRecord transactionRecord = new TransactionRecord();
+        AccountService accountService = new AccountService(consoleMock, transactionRecord, bankStatement);
+        accountService.deposit1(1000);
+        accountService.deposit1(2000);
+        accountService.withdraw1(500);
+        accountService.printStatement1();
 
 
         InOrder inOrder = inOrder(consoleMock);
