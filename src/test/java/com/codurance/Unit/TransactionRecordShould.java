8e08bbea60b101e99018a62aa27fc4dc8e329bec
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-08-06 15:46:28 +0100

    Refactoring - tidy tests

--- src/test/java/com/codurance/Unit/TransactionRecordShould.java
+++ src/test/java/com/codurance/Unit/TransactionRecordShould.java
@@ -29,14 +29,6 @@ public class TransactionRecordShould {
     }
 
     @Test
-    void add_deposit() {
-        TransactionRecord transactionRecord = new TransactionRecord(transactionsListMock, clockMock);
-        transactionRecord.addDeposit(AMOUNT);
-
-        verify(transactionsListMock).add(isA(Transaction.class));
-    }
-
-    @Test
     void add_withdraw() {
         TransactionRecord transactionRecord = new TransactionRecord(transactionsListMock, clockMock);
         transactionRecord.addWithdraw(AMOUNT);
@@ -45,39 +37,20 @@ public class TransactionRecordShould {
     }
 
     @Test
-    void add_deposit_transaction_with_correct_amount() {
-        TransactionRecord transactionRecord = new TransactionRecord(transactionsListMock, clockMock);
-        transactionRecord.addDeposit(AMOUNT);
-
-        ArgumentCaptor<Transaction> argument = ArgumentCaptor.forClass(Transaction.class);
-        verify(transactionsListMock).add(argument.capture());
-
-        assertThat(argument.getValue().amount()).isEqualTo(AMOUNT);
-    }
-
-    @Test
-    void add_deposit_transaction_with_correct_balance() {
+    void add_deposit_with_valid_transaction() {
+        final LocalDateTime dateTime = LocalDateTime.of(2019, Month.AUGUST, 02, 15, 19);
         int balance = 30;
+
+        when(clockMock.now()).thenReturn(dateTime);
+
         TransactionRecord transactionRecord = new TransactionRecord(transactionsListMock, clockMock);
         transactionRecord.addDeposit(AMOUNT);
 
         ArgumentCaptor<Transaction> argument = ArgumentCaptor.forClass(Transaction.class);
         verify(transactionsListMock).add(argument.capture());
 
+        assertThat(argument.getValue().date()).isEqualTo(dateTime);
+        assertThat(argument.getValue().amount()).isEqualTo(AMOUNT);
         assertThat(argument.getValue().balance()).isEqualTo(balance);
     }
-    @Test
-    void add_deposit_transaction_with_correct_date() {
-        when(clockMock.now()).thenReturn(LocalDateTime.of(2019, Month.AUGUST, 02, 15, 19));
-
-        TransactionRecord transactionRecord = new TransactionRecord(transactionsListMock, clockMock);
-        transactionRecord.addDeposit(AMOUNT);
-
-        ArgumentCaptor<Transaction> argument = ArgumentCaptor.forClass(Transaction.class);
-        verify(transactionsListMock).add(argument.capture());
-
-        assertThat(argument.getValue().date())
-                .isEqualTo(LocalDateTime.of(2019, Month.AUGUST, 02, 15, 19));
-    }
-
 }
