69d461d4c46f33a28725b8a0da3b55deed7d4e2c
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-08-06 14:56:55 +0100

    Passing test for addDeposit with correct balance

--- src/main/java/com/codurance/TransactionRecord.java
+++ src/main/java/com/codurance/TransactionRecord.java
@@ -4,17 +4,21 @@ import java.util.*;
 
 public class TransactionRecord implements Iterable<Transaction>{
     private final List<Transaction> transactions;
+    private int balance;
 
     public TransactionRecord(List<Transaction> transactionList) {
         this.transactions = transactionList;
+        balance = 0;
     }
 
     public TransactionRecord() {
         this.transactions = new ArrayList<>();
+        balance = 0;
     }
 
     public void addDeposit(int amount) {
-        transactions.add(new Transaction("02/08/2019", amount, 0));
+        balance += amount;
+        transactions.add(new Transaction("02/08/2019", amount, balance));
     }
 
     public void addWithdraw(int amount) {
